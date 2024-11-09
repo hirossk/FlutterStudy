0db5e3bba66e033dbf5a724523e1634718655255
Hiroyuki Sasaki <sasaki@yoshida-g.ac.jp>
2024-11-09 12:01:52 +0900

    error回避

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -5,6 +5,7 @@
 // gestures. You can also use WidgetTester to find child widgets in the widget
 // tree, read text, and verify that the values of widget properties are correct.
 
+import 'package:app01/step/sub0.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
